966d1021dea5f87237a59f6e3ea01128005fadcb
Arbey Dzib <arbeydzib1@gmail.com>
2025-06-20 16:13:31 -0600

    cors enabled

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.api.v1.routers import users, predict
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Tu API",
@@ -7,7 +8,13 @@ app = FastAPI(
     version="1.0",
     docs_url="/",  # Swagger en la raíz
     redoc_url=None  # Deshabilita Redoc
+    
 )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'])
+
+
 
 app.include_router(users.router, prefix="/api/v1")
 app.include_router(predict.router, prefix="/api/v1")
