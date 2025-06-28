6f65df525a7fa220271492c3bc3c65ecc8dad62c
GuillermoSM33 <guillermo.jesus.garcia.canul@gmail.com>
2025-06-28 12:44:37 -0500

    FEAT: ADD AFFECTS PERFORMANCE ACADEMIC

--- app/api/v1/services/calculate/calculate_form_service.py
+++ app/api/v1/services/calculate/calculate_form_service.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-from sklearn.linear_model import LinearRegression 
+from sklearn.linear_model import LinearRegression, LogisticRegression 
 from app.api.v1.classes.form_input import FormInputUser
 from app.api.v1.services.data_service import load_csv
 
@@ -27,6 +27,20 @@ def estimate_mental_health_and_addiction_score(data: FormInputUser):
         """ Esta variable se calcula en base a la predicción del modelo """
         addicted_score = (normalized_usage_hours + (10 - predicted_mental_score)) / 2
 
+        """ Calculamos el rendimiento académico """
+        X_academic = df[['Mental_Health_Score', 'Addicted_Score']]
+        Y_academic = df['Affects_Academic_Performance'].apply(lambda x: 1 if str(x).lower() in ['yes', 'true', '1'] else 0)
+
+        """ Entrenamos el modelo de regresión lineal para el rendimiento académico """
+        model_academic = LogisticRegression()
+        model_academic.fit(X_academic, Y_academic)
+
+        """ Realizamos la predicción del rendimiento académico """
+        prediction_clf = model_academic.predict([[predicted_mental_score, addicted_score]])[0]
+        prob = model_academic.predict_proba([[predicted_mental_score, addicted_score]])[0][1]
+
+        performance_impact = True if prediction_clf == 1 else False
+
         """ Las variables de salida, son el estado de salud mental y el puntaje de adicción """
 
         return {
@@ -41,7 +55,9 @@ def estimate_mental_health_and_addiction_score(data: FormInputUser):
                 "Alta adicción" if addicted_score > 7 else
                 "Adicción moderada" if addicted_score >= 5 else
                 "Baja adicción"
-            )
+            ),
+            "Affects_Academic_Performance": performance_impact,
+            "Academic_Performance_Probability": round(prob, 2),
         }
 
     except Exception as e:
